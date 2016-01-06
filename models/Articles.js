45c5f91af3bcb15fd70d2dbb6ef53f42d32b54bc
James Traver <jtraverjr@gmail.com>
2016-01-06 10:12:47 -0600

    db

---
+++ models/Articles.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://localhost/fakeblog';
+
+mongoose.connect(connectionString);
+
+mongoose.connection.on('connected', function() {
+  console.log('Das fake blog is publishing propaganda!');
+});
+
+
+mongoose.connection.on('error', function(err) {
+  console.log(err);
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Das fake blog is going DOWN!!!');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// mongoose helpers
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mongoose Children' });
 });
 
 module.exports = router;
